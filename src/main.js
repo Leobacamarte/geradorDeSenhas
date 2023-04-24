c4cbd90b5a017b2af2955bce5bc1fca795f270ee
Leonardo Goncalves <martinsleo1999@gmail.com>
2023-04-24 13:55:28 -0300

    adiciona funcionalidade de copiar a senha

--- src/main.js
+++ src/main.js
@@ -1,9 +1,17 @@
 import { nanoid } from 'nanoid'
 
-const passwordBtn = document.querySelector('button');
+const passwordBtn = document.querySelector('.generateBtn');
 const displayPassword = document.querySelector('h2');
+const copyPassword = document.querySelector('.copyBtn');
 
 passwordBtn.addEventListener('click', () => {
   const randomPassword = nanoid();
   displayPassword.innerHTML = randomPassword;
+});
+
+copyPassword.addEventListener('click', () => {
+  const password = displayPassword.innerHTML;
+  navigator.clipboard.writeText(password);
+  alert('A senha foi copiada');
+  displayPassword.innerHTML = '...';
 });
